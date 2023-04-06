90f47b1f57f2acbcb37ab5cde0f5e5ec0ac7de64
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-04-06 17:05:57 +0500

    - Removed unnecessary code

--- Sources/SmilesFontsManager/Typography/Typography.swift
+++ Sources/SmilesFontsManager/Typography/Typography.swift
@@ -95,123 +95,13 @@ public struct Typography {
         guard let fontName = self.fontName, let pointSize = self.pointSize else {
             return nil
         }
-        return font(fontName, pointSize: pointSize)
+        return UIFont(name: fontName, size: CGFloat(pointSize))
     }
     
     /// Convenience method for retrieving the line height.
     @MainActor
     public func lineHeight() -> CGFloat? {
         return font()?.lineHeight
-    }
-    
-}
-
-private extension Typography {
-    
-    private func resolvedMaxPointSize() -> Float? {
-        return maximumPointSize
-    }
-    
-    private func resolvedMinPointSize() -> Float? {
-        return minimumPointSize
-    }
-    
-    /// Resolves font definitions defined in configuration to the system font with the specified `UIFont.Weight`.
-    /// e.g. 'system-ultra-light' resolves to the system font with `UIFont.Weight` of `.ultraLight`.
-    private func resolveSystemFont(_ fontName: String, pointSize: Float) -> UIFont? {
-        let lowerCasedFontName = fontName.lowercased()
-        let points = CGFloat(pointSize)
-        if let unweightedFont = unweightedSystemFont(fontName, pointSize: points) {
-            return unweightedFont
-        }
-        let lowerCasedSystemFontName = type(of: self).systemFontName.lowercased()
-        let lowerCasedMonospacedDigitSystemFontName = type(of: self).monospacedDigitSystemFontName.lowercased()
-        let fontWeights = type(of: self).fontWeightMap
-        for (fontWeightName, fontWeight) in fontWeights {
-            let lowercasedFontWeightName = fontWeightName.lowercased()
-            let systemFontWithWeightName = "\(lowerCasedSystemFontName)-\(lowercasedFontWeightName)"
-            if lowerCasedFontName == systemFontWithWeightName {
-                return UIFont.systemFont(ofSize: points, weight: fontWeight)
-            }
-            let monospacedDigitFontWithWeightName =
-            "\(lowerCasedMonospacedDigitSystemFontName)-\(lowercasedFontWeightName)"
-            if #available(iOS 9.0, *), lowerCasedFontName == monospacedDigitFontWithWeightName {
-                return UIFont.monospacedDigitSystemFont(ofSize: points, weight: fontWeight)
-            }
-        }
-        return nil
-    }
-    
-    /// Scales `UIFont` using a `UIFontMetrics` obtained from a `UIFont.TextStyle`.
-    private func scaleUsingFontMetrics(
-        _ fontName: String,
-        pointSize: Float,
-        textStyle: UIFont.TextStyle,
-        contentSizeCategory: UIContentSizeCategory
-    ) -> UIFont? {
-        guard var newFont = font(fontName, pointSize: pointSize) else {
-            return nil
-        }
-        let traitCollection = UITraitCollection(preferredContentSizeCategory: contentSizeCategory)
-        if let maxPointSize = resolvedMaxPointSize() {
-            newFont = UIFontMetrics.default.scaledFont(
-                for: newFont,
-                maximumPointSize: CGFloat(maxPointSize),
-                compatibleWith: traitCollection
-            )
-        } else {
-            newFont = UIFontMetrics.default.scaledFont(for: newFont, compatibleWith: traitCollection)
-        }
-        if let minimumPointSize = resolvedMinPointSize(), newFont.pointSize < CGFloat(minimumPointSize) {
-            return font(fontName, pointSize: minimumPointSize)
-        } else {
-            return newFont
-        }
-    }
-    
-    /// Scales `UIFont` using a step size * multiplier increasing in-line with the `UIContentSizeCategory` value.
-    private func scaleUsingStepping(_ fontName: String, pointSize: Float, contentSize: UIContentSizeCategory)
-    -> UIFont? {
-        // No scaling if the UIContentSizeCategory cannot be found in map.
-        let defaultContentSizeCategoryScaling: Float = 0.0
-        let contentSizeCategoryScaling = type(of: self).contentSizeCategoryMap[contentSize]
-        ?? defaultContentSizeCategoryScaling
-        let stepSizeMultiplier: Float = 1.0
-        let stepSize: Float = 2.0
-        var newPointSize = pointSize + (stepSize * stepSizeMultiplier * contentSizeCategoryScaling)
-        if let minimumPointSize = resolvedMinPointSize(), newPointSize < minimumPointSize {
-            newPointSize = minimumPointSize
-        }
-        if let maximumPointSize = resolvedMaxPointSize(), maximumPointSize < newPointSize {
-            newPointSize = maximumPointSize
-        }
-        return font(fontName, pointSize: newPointSize)
-    }
-    
-    private func font(_ fontName: String, pointSize: Float) -> UIFont? {
-        resolveSystemFont(fontName, pointSize: pointSize) ?? UIFont(name: fontName, size: CGFloat(pointSize))
-    }
-    
-    /// Resolves font entries in configuration to the following `UIFont` methods:
-    /// System -> systemFont(ofSize: CGFloat) -> UIFont
-    /// BoldSystem -> boldSystemFont(ofSize: CGFloat) -> UIFont
-    /// ItalicSystem -> italicSystemFont(ofSize: CGFloat) -> UIFont
-    private func unweightedSystemFont(_ fontName: String, pointSize: CGFloat) -> UIFont? {
-        let lowerCasedFontName = fontName.lowercased()
-        let lowerCasedSystemFontName = type(of: self).systemFontName.lowercased()
-        let lowerCasedBoldSystemFontName = type(of: self).boldSystemFontName.lowercased()
-        let lowerCasedItalicSystemFontName = type(of: self).italicSystemFontName.lowercased()
-        let points = CGFloat(pointSize)
-        switch lowerCasedFontName {
-        case lowerCasedSystemFontName:
-            return UIFont.systemFont(ofSize: points)
-        case lowerCasedBoldSystemFontName:
-            return UIFont.boldSystemFont(ofSize: points)
-        case lowerCasedItalicSystemFontName:
-            return UIFont.italicSystemFont(ofSize: points)
-        default:
-            return nil
-        }
     }
     
 }
