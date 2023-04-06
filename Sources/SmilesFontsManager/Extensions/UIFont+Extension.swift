502deea148f6e9ffcd4bda95e852ecc6185ffed6
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-04-06 16:10:10 +0500

    - Made font style public

--- Sources/SmilesFontsManager/Extensions/UIFont+Extension.swift
+++ Sources/SmilesFontsManager/Extensions/UIFont+Extension.swift
@@ -9,24 +9,24 @@ import UIKit
 
 extension UIFont.TextStyle {
     
-    static let headline1 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline1.rawValue)
-    static let headline2 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline2.rawValue)
-    static let headline3 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline3.rawValue)
-    static let headline4 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline4.rawValue)
-    static let headline5 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline5.rawValue)
+    public static let headline1 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline1.rawValue)
+    public static let headline2 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline2.rawValue)
+    public static let headline3 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline3.rawValue)
+    public static let headline4 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline4.rawValue)
+    public static let headline5 = UIFont.TextStyle(rawValue: TypographyFontStyles.headline5.rawValue)
     
-    static let title1 = UIFont.TextStyle(rawValue: TypographyFontStyles.title1.rawValue)
-    static let title2 = UIFont.TextStyle(rawValue: TypographyFontStyles.title2.rawValue)
-    static let title3 = UIFont.TextStyle(rawValue: TypographyFontStyles.title3.rawValue)
+    public static let title1 = UIFont.TextStyle(rawValue: TypographyFontStyles.title1.rawValue)
+    public static let title2 = UIFont.TextStyle(rawValue: TypographyFontStyles.title2.rawValue)
+    public static let title3 = UIFont.TextStyle(rawValue: TypographyFontStyles.title3.rawValue)
     
-    static let subtitle1 = UIFont.TextStyle(rawValue: TypographyFontStyles.subtitle1.rawValue)
-    static let subtitle2 = UIFont.TextStyle(rawValue: TypographyFontStyles.subtitle2.rawValue)
+    public static let subtitle1 = UIFont.TextStyle(rawValue: TypographyFontStyles.subtitle1.rawValue)
+    public static let subtitle2 = UIFont.TextStyle(rawValue: TypographyFontStyles.subtitle2.rawValue)
     
-    static let body1 = UIFont.TextStyle(rawValue: TypographyFontStyles.body1.rawValue)
-    static let body2 = UIFont.TextStyle(rawValue: TypographyFontStyles.body2.rawValue)
-    static let body3 = UIFont.TextStyle(rawValue: TypographyFontStyles.body3.rawValue)
+    public static let body1 = UIFont.TextStyle(rawValue: TypographyFontStyles.body1.rawValue)
+    public static let body2 = UIFont.TextStyle(rawValue: TypographyFontStyles.body2.rawValue)
+    public static let body3 = UIFont.TextStyle(rawValue: TypographyFontStyles.body3.rawValue)
     
-    static let label1 = UIFont.TextStyle(rawValue: TypographyFontStyles.label1.rawValue)
-    static let label2 = UIFont.TextStyle(rawValue: TypographyFontStyles.label2.rawValue)
+    public static let label1 = UIFont.TextStyle(rawValue: TypographyFontStyles.label1.rawValue)
+    public static let label2 = UIFont.TextStyle(rawValue: TypographyFontStyles.label2.rawValue)
     
 }
