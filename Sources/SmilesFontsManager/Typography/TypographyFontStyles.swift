ba62e2d58e766887961dd59d6593bf7e5537ffa7
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-07-02 12:25:14 +0500

    - Added smilesLabel3 font typography

--- Sources/SmilesFontsManager/Typography/TypographyFontStyles.swift
+++ Sources/SmilesFontsManager/Typography/TypographyFontStyles.swift
@@ -12,7 +12,7 @@ public enum TypographyFontStyles: String, CaseIterable {
     case smilesHeadline1, smilesHeadline2, smilesHeadline3, smilesHeadline4, smilesHeadline5
     case smilesTitle1, smilesTitle2, smilesTitle3
     case smilesBody1, smilesBody2, smilesBody3, smilesBody4
-    case smilesLabel1, smilesLabel2
+    case smilesLabel1, smilesLabel2, smilesLabel3
     
     func getTypography() -> Typography {
         switch self {
@@ -44,6 +44,8 @@ public enum TypographyFontStyles: String, CaseIterable {
             return Typography(name: self.rawValue, fontName: SmilesFontsManager.defaultAppFont.getFontName(fontStyle: .book), fontSize: 11, letterSpacing: 8, lineHeight: 14)
         case .smilesLabel2:
             return Typography(name: self.rawValue, fontName: SmilesFontsManager.defaultAppFont.getFontName(fontStyle: .book), fontSize: 12, letterSpacing: 8, lineHeight: 16)
+        case .smilesLabel3:
+            return Typography(name: self.rawValue, fontName: SmilesFontsManager.defaultAppFont.getFontName(fontStyle: .book), fontSize: 8, letterSpacing: 8, lineHeight: 14)
         }
     }
     
